8354df776137e4c1c1421e19a2a6003e8c8db091
Kingsten <kingsten.banh@gmail.com>
2015-02-07 13:22:21 -0700

    fix typo and correctly place of files

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var auth = require('./auth.js');
 var products = require('./products.js');
-var user = require('./user.js');
+var user = require('./users.js');
 
 // Routes that can be accessed by any one
 router.post('/login', auth.login);
@@ -12,7 +12,7 @@ router.post('/login', auth.login);
 router.get('/api/v1/products', products.getAll);
 router.get('/api/v1/product/:id', products.getOne);
 router.post('/api/v1/product/', products.create);
-router.put('/api/v1/product/:id', product.update);
+router.put('/api/v1/product/:id', products.update);
 router.delete('/api/v1/product/:id', products.delete);
 
 // Routes that can be accessed only by authenticated & authorized users
